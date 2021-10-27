1ed3aab1923bd4c648248bcf9c9027fc72066d6e
AfsarHossain <afsar291722@gmail.com>
2021-10-28 00:17:33 +0600

    Link List Delete Addes

---
+++ 2.Data Structures/2.Link List/4.Nth No Delete/Delete.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+struct Node *head = NULL;
+
+void add(int val)
+{
+    struct Node *newNode = new Node();
+
+    newNode->data = val;
+    newNode->next = NULL;
+
+    if(head == NULL)
+    {
+        head = newNode;
+    }
+    else
+    {
+        struct Node *lastNode = head;
+        while(lastNode->next != NULL)
+        {
+            lastNode = lastNode->next;
+        }
+        lastNode->next = newNode;
+    }
+}
+
+void deleteNode(struct Node **head, int key)
+{
+    struct Node *temp;
+
+    if((*head)->data == key)
+    {
+        temp = *head;
+        *head = (*head)->next;
+        free(temp);
+    }
+    else
+    {
+        struct Node *current = *head;
+
+        while( current->next != NULL)
+        {
+            if(current->next->data == key)
+            {
+                temp = current->next;
+                current->next = current->next->next;
+                free(temp);
+                break;
+            }
+            else
+            {
+                current = current->next;
+            }
+        }
+    }
+}
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" -> ";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+int main()
+{
+    for(int i=10; i<=100; i+=10)
+    {
+        add(i);
+    }
+
+    cout<<"-------------------  ADDED  -----------------"<<endl;
+    /*
+    struct Node *temp = head;
+    while( temp != NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    */
+    printList(head);
+
+    cout<<"-------------------  DELETE  -----------------"<<endl;
+    deleteNode(&head, 50);
+    printList(head);
+
+    deleteNode(&head, 80);
+    printList(head);
+}
+
